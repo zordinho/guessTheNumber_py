31206d80b0b097d27dd3f7744bd5a7b8bc29300c
zordinho <zordi7@gmail.com>
2018-08-07 21:35:14 +1000

    Range_was_changed

---
+++ guessTheNumber.py
@@ -0,0 +1,31 @@
+import random
+minimum = 0
+maximum = 1000
+rand = random.randint(minimum, maximum)
+print("Guess the number from range [", minimum, maximum,"]")
+while True:
+    try:
+        guessedNumber = int(input())
+        break
+    except ValueError:
+        print("Oops, put the valid number! Try again!")
+count = 1
+while rand != guessedNumber:
+    if guessedNumber < rand:
+        print("The figure is higher, keep guessing")
+        while True:
+            try:
+                guessedNumber = int(input())
+                break
+            except ValueError:
+                print("Oops, put the valid number! Try again!")
+    else:
+        print("The figure is smaller, keep guessing")
+        while True:
+            try:
+                guessedNumber = int(input())
+                break
+            except ValueError:
+                print("Oops, put the valid number! Try again!")
+    count = count +1
+print("Well done you guessed the number. Your score is",count)
